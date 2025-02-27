fa22ed0a4c7a3279ca704533d407f4fac393ef60
Neelkanth Rawat <neelkanthrawat3992@gmail.com>
2025-02-27 14:23:17 +0100

    add line entailment cancelling operator and switch posn of  embedding and context

--- Prompt.py
+++ Prompt.py
@@ -14,14 +14,14 @@ class Prompt:
             SECOND PIECE:
             """,
             "cb": """INSTRUCTION:
-            Given a sentence (CONTEXT) and its associated Embedding type (such as "conditional" or "modal"), generate the most likely (TARGET) sentence that logically follows.
+            Given a sentence (CONTEXT) and its associated Embedding type (entailment cancelling operator), generate the most likely sentence (TARGET) that logically follows.
             Ensure that the generated sentence aligns with the specified embedding type.
-
-            CONTEXT: 
-            {first_piece}
 
             EMBEDDING: 
             {label}
+
+            CONTEXT: 
+            {first_piece}
 
             TARGET:
             """,
