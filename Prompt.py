1fdb428c63e076c2320f84b76fb19e0b340655a4
Neelkanth Rawat <neelkanthrawat3992@gmail.com>
2025-03-02 14:26:08 +0100

    finetune prompt

--- Prompt.py
+++ Prompt.py
@@ -16,7 +16,7 @@ class Prompt:
         }
 
         self.guided_prompts = {
-            "winogrande": """INSTRUCTION: You are provided with the FIRST PIECE of the validation split of the Winograd Schema Challenge (WSC) dataset. Finish the SECOND PIECE of the instance as EXACTLY appeared in the dataset. Only rely on the original form of the instance in the dataset to finish the SECOND PIECE.
+            "winogrande": """INSTRUCTION: You are provided with the FIRST PIECE of the validation split of the Winograd Schema Challenge (WSC) dataset. Finish the SECOND PIECE of the instance as EXACTLY appeared in the dataset such that these two pieces becomes a single instance. Only rely on the original form of the instance in the dataset to finish the SECOND PIECE. Don't respond with the FIRST PIECE or the instruction.
             FIRST PIECE: {first_piece} SECOND PIECE:""",
             "cb": """INSTRUCTION: You are provided with the CONTEXT and EMBEDDING (entailment cancelling operator) from the CommitmentBank dataset. Finish the TARGET setnence as appeared EXACTLY in the dataset. TARGET MUST EXACTLY MATCH the target in the dataset!!! 
             CONTEXT: {first_piece}
