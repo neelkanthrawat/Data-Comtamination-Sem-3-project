7e89874dceecd1c5e3d38220a12df6eccfa26799
Neelkanth Rawat <neelkanthrawat3992@gmail.com>
2025-03-03 16:53:18 +0100

    fix

--- run.py
+++ run.py
@@ -156,7 +156,7 @@ def main():
         encoded_prompt = tokenizer(
             formatted_prompt, return_tensors="pt", add_special_tokens=True
         )
-        #print(f'encoded_prompt {encoded_prompt}')
+        print(f'encoded_prompt {encoded_prompt}')
         start_index_answer = len(encoded_prompt['input_ids'])
         print(f'start index is: {start_index_answer}')
 
