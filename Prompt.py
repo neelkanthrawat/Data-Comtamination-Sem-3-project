6ad5262c472e6885c1430c244f4e7c37505d214d
Neelkanth Rawat <neelkanthrawat3992@gmail.com>
2025-03-03 16:27:36 +0100

    SOME CHANGES ALPACA

--- Prompt.py
+++ Prompt.py
@@ -16,13 +16,15 @@ class Prompt:
         }
 
         self.guided_prompts = {
-            "winogrande": """### INSTRUCTION: 
-            You are provided with the FIRST PIECE of the validation split of the Winograd Schema Challenge (WSC) dataset. Finish the SECOND PIECE of the instance as EXACTLY appeared in the dataset such that these two pieces (CONTEXT + TARGET) becomes a single instance. Only rely on the original form of the TARGET instance in the dataset to finish the SECOND PIECE. DON'T JUST REPEAT the FIRST PIECE or the INSTRUCTION!
+            "winogrande": """
+            Below is an instruction that describes a task, paired with an input that provides further context.
+            Write a response that appropriately completes the request.
 
+            ### INSTRUCTION:
+            You are provided with the FIRST PIECE of the validation split of the Winograd Schema Challenge (WSC) dataset. Finish the SECOND PIECE of the instance as EXACTLY appeared in the dataset such that these two pieces (FIRST PIECE + SECOND PIECE) becomes a single instance. Only rely on the original form of the FIRST PIECE in the dataset to finish the SECOND PIECE. DON'T JUST REPEAT the FIRST PIECE or the INSTRUCTION!
 
             ### FIRST PIECE: 
             {first_piece} 
-            
             
             ### SECOND PIECE:
             """,
