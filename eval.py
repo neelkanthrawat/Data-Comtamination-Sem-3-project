73dea5f5f1f596e5ebb686147a7570b1d1e8ab17
kim <kim@tg-lingemann.de>
2025-03-04 17:32:15 +0100

    wefs

--- eval.py
+++ eval.py
@@ -13,9 +13,15 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Run the model")
 
     parser.add_argument(
-        "--pred",
+        "--guided",
         type=str,
-        help="Path to the predictions file",
+        help="Path to the guided predictions file",
+    )
+
+    parser.add_argument(
+        "--unguided",
+        type=str,
+        help="Path to the unguided predictions file",
     )
 
     args = parser.parse_args()
@@ -76,10 +82,52 @@ def calc_scores(path: str):
     results_df.to_csv(res_path, index=False, sep=";")
     print(f"Results saved to {res_path}")
 
+    return results_df, res_path
+
+
+def calc_differences(results_df_guided, results_df_unguided):
+    if type(results_df_guided) == str:
+        with open(results_df_guided, "r") as f:
+            results_df_guided = pd.read_csv(f, sep=";")
+    if type(results_df_unguided) == str:
+        with open(results_df_unguided, "r") as f:
+            results_df_unguided = pd.read_csv(f, sep=";")
+
+    diff_df = pd.DataFrame(
+        columns=[
+            "Index",
+            "BLEURT guided",
+            "BLEURT unguided",
+            "BLEURT_diff",
+            "ROUGEL guided",
+            "ROUGEL unguided",
+            "ROUGEL_diff",
+        ]
+    )
+
+    diff_df["BLEURT_diff"] = results_df_guided["BLEURT"] - results_df_unguided["BLEURT"]
+    diff_df["ROUGEL_diff"] = results_df_guided["ROUGEL"] - results_df_unguided["ROUGEL"]
+
+    diff_df["Index"] = results_df_guided["Index"]
+    diff_df["BLEURT guided"] = results_df_guided["BLEURT"]
+    diff_df["BLEURT unguided"] = results_df_unguided["BLEURT"]
+    diff_df["ROUGEL guided"] = results_df_guided["ROUGEL"]
+    diff_df["ROUGEL unguided"] = results_df_unguided["ROUGEL"]
+
+    res_path = os.path.join("results", f"differences.csv")
+    diff_df.to_csv(res_path, index=False, sep=";")
+    print(f"Differences saved to {res_path}")
+
+
+def ICL_prompting():
+    pass
+
 
 def main():
     args = parse_args()
-    calc_scores(args.pred)
+    results_df_guided, res_path_guided = calc_scores(args.guided)
+    results_df_unguided, res_path_unguided = calc_scores(args.unguided)
+    calc_differences(results_df_guided, results_df_unguided)
 
 
 if __name__ == "__main__":
