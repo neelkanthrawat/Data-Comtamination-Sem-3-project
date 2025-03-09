991a4ede286766960426f940916f5f56b672eaf0
Neelkanth Rawat <neelkanthrawat3992@gmail.com>
2025-03-09 15:15:47 +0100

    change time from 30min to 14 min

--- run_wsc_unguided.sh
+++ run_wsc_unguided.sh
@@ -3,7 +3,7 @@
 # Job name
 #SBATCH --job-name=DataContam               # TODO: adjust job name
 
-#SBATCH --time=00:30:00              # Job time limit (30 minutes)
+#SBATCH --time=00:14:00              # Job time limit (30 minutes)
 #SBATCH --ntasks=1                   # Total number of tasks
 #SBATCH --gres=gpu:1                 # Request 2 GPUs
 #SBATCH --cpus-per-task=1            # Number of CPU cores per task
