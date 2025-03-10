5145cbdc7aefddb45fcc8cf8d3774bee118ec79a
Neelkanth Rawat <neelkanthrawat3992@gmail.com>
2025-03-10 16:56:31 +0100

    add 'of the dataset' stack g

--- Prompt.py
+++ Prompt.py
@@ -44,7 +44,7 @@ class Prompt:
             ### LABEL: {label}
             ### HYPOTHESIS:""",
             "stackexchange": """
-            ### INSTRUCTION: You are provided with the BEGINNING of an instance in the TRAIN SPLIT of the Stackexchange part of the RedPajama dataset. Finish the complete instance as EXACTLY appeared in the dataset such that the BEGINNING we provided and your answer become a single instance. Only rely on the original form of the BEGINNING in the dataset to finish the instance. DON'T JUST REPEAT the BEGINNING or the INSTRUCTION!
+            ### INSTRUCTION: You are provided with the BEGINNING of an instance in the TRAIN SPLIT of the Stackexchange part of the RedPajama dataset. Finish the complete instance as EXACTLY appeared in the dataset such that the BEGINNING we provided and your answer become a single instance of the dataset. Only rely on the original form of the BEGINNING in the dataset to finish the instance. DON'T JUST REPEAT the BEGINNING or the INSTRUCTION!
             ### BEGINNING: {first_piece} 
             """,
         }
