b2baf308c2509b893c2fc49eb5150aeddefc71d6
kim <kim@tg-lingemann.de>
2025-03-14 14:48:54 +0100

    fix scores to diffs

--- Statistics.py
+++ Statistics.py
@@ -98,7 +98,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    calculate_statistics(args.scores, args.icl)
+    calculate_statistics(args.diffs, args.icl)
 
 
 if __name__ == "__main__":
