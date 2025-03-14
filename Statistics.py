36edf6caa9516f4827f798bfac673c50c149d67d
kim <kim@tg-lingemann.de>
2025-03-14 13:35:50 +0100

    add basic statistics

--- Statistics.py
+++ Statistics.py
@@ -1,5 +1,26 @@
+import pandas as pd
+import Path
+import os
+
+HOME = Path.home()
+PROJECT_DIR = os.path.join(HOME, "Data-Comtamination-Sem-3-project")
+
+
 def calculate_statistics(scores, icl):
-    pass
+    with open(scores, "r") as f:
+        scores = pd.read_csv(f)
+
+    with open(icl, "r") as f:
+        icl = pd.read_csv(f)
+
+    res_dir = os.path.join(PROJECT_DIR, "results")
+    if not os.path.exists(res_dir):
+        os.makedirs(res_dir)
+
+    res_path = os.path.join(res_dir, scores.split("."), "_statistics.csv")
+    scores.describe().to_csv(res_path)
+    res_path = os.path.join(res_dir, scores.split("."), "_statistics.csv")
+    icl.describe().to_csv(res_path)
 
 
 def parse_args():
