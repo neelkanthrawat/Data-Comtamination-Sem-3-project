c070cf4694b17382544f27ae185ce9984dd61b46
kim <kim@tg-lingemann.de>
2025-03-26 15:28:53 +0100

    add replication study stuff

---
+++ replication_study.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import os
+import evaluate
+
+
+def calculate_bleurt(preds, refs):
+    bleurt = evaluate.load("bleurt", module_type="metric", checkpoint="BLEURT-20")
+    bleurt_score = bleurt.compute(predictions=preds, references=refs)
+    return bleurt_score
+
+
+def calculate_rouge(preds, refs):
+    rouge = evaluate.load("rouge")
+    rouge_score = rouge.compute(predictions=preds, references=refs)
+    return rouge_score
+
+
+results_dir = "time-travel-in-llms-main/results/"
+paths = []
+for dirpath, _, filenames in os.walk(results_dir):
+    for file in filenames:
+        if file.endswith(".csv"):
+            paths.append(os.path.join(dirpath, file))
+
+for file_path in paths:
+    with open(file_path) as f:
+        df = pd.read_csv(f)
+        bleurt_score_guided = calculate_bleurt(
+            preds=df["generated_guided_completion"].tolist(),
+            refs=df["second_piece"].tolist(),
+        )
+        rouge_score_guided = calculate_rouge(
+            preds=df["generated_guided_completion"].tolist(),
+            refs=df["second_piece"].tolist(),
+        )
+
+        bleurt_score_unguided = calculate_bleurt(
+            preds=df["generated_general_completion"].tolist(),
+            refs=df["second_piece"].tolist(),
+        )
+        bleurt_score_guided = calculate_rouge(
+            preds=df["generated_general_completion"].tolist(),
+            refs=df["second_piece"].tolist(),
+        )
+
+        print(f"File: {file_path}")
+        print(
+            f"Our BLEURT Score Guided: {bleurt_score_guided}, theirs: {df['bleurt_score_for_guided_completion'].tolist()}"
+        )
+        print(
+            f"Our ROUGE Score Guided: {rouge_score_guided}, theirs: {df['rouge_score_for_guided_completion'].tolist()}"
+        )
+        print(
+            f"Our BLEURT Score Unguided: {bleurt_score_unguided}, theirs: {df['bleurt_score_for_general_completion'].tolist()}"
+        )
+        print(
+            f"Our ROUGE Score Unguided: {rouge_score_guided}, theirs: {df['rouge_score_for_general_completion'].tolist()}"
+        )
